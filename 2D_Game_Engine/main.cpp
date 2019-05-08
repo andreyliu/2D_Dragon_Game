60186af37aaba8aebb3fe284cf94a3d57c3d6bec
andreliuburningcrust <yuqiliu@andrew.cmu.edu>
2019-05-08 14:33:01 -0400

    Initial Commit

---
+++ 2D_Game_Engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2D_Game_Engine
+//
+//  Created by Yuqi Liu on 5/8/19.
+//  Copyright © 2019 Yuqi Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
