93dd908216e7862b0758c6ba1f51552b632107c9
andreliuburningcrust <yuqiliu@andrew.cmu.edu>
2019-05-12 18:22:34 -0400

    flame and dragon 2.1

---
+++ 2D_Game_Engine/Enemies.cpp
@@ -0,0 +1,9 @@
+//
+//  Enemies.cpp
+//  2D_Game_Engine
+//
+//  Created by Yuqi Liu on 5/12/19.
+//  Copyright © 2019 Yuqi Liu. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ 2D_Game_Engine/Enemies.h
@@ -0,0 +1,13 @@
+//
+//  Enemies.h
+//  2D_Game_Engine
+//
+//  Created by Yuqi Liu on 5/12/19.
+//  Copyright © 2019 Yuqi Liu. All rights reserved.
+//
+
+#ifndef Enemies_h
+#define Enemies_h
+
+
+#endif /* Enemies_h */
